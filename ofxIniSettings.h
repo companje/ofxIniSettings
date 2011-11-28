13da7c65d5c88dbc0f74b75a2d5dfea1bd67c482
Rick Companje <rick@companje.nl>
2011-11-28 14:55:24 +0100

    first commit

---
+++ ofxIniSettings.h
@@ -0,0 +1,45 @@
+#ifndef OFX_INISETTINGS_H
+#define OFX_INISETTINGS_H
+
+#include <fstream>
+#include <iostream>
+#include <map>
+
+#include "ofMain.h"
+#include "ofxExtras.h"
+
+class ofxIniSettings {
+public:
+
+    ofxIniSettings() {}
+    ofxIniSettings(string filename) { load(filename); }
+
+    bool load(string filename, bool clearFirst=false); // you can call multiple times with different files, incremental
+    bool has(string key) { return keys[key]!=""; };
+    void clear();
+
+    map<string,string> keys;
+    string outputFilename;
+
+    //getters
+    int get(string key, int defaultValue);
+    bool get(string key, bool defaultValue);
+    float get(string key, float defaultValue);
+    string get(string key, string defaultValue);
+    string get(string key, const char* defaultValue);
+    ofVec2f get(string key, ofVec2f defaultValue);
+    ofVec3f get(string key, ofVec3f defaultValue);
+    ofVec4f get(string key, ofVec4f defaultValue);
+    ofRectangle get(string key, ofRectangle defaultValue);
+    ofQuaternion get(string key, ofQuaternion defaultValue);
+    
+    //setters
+    void setString(string id, string value);
+
+    template <typename T> void set(string id, string key, T value) { set(id=="" ? key : (id+"."+key), value); } //returntype void
+    template <typename T> void set(string key, T value) { setString(key, ofxToString(value)); } //returntype void
+
+};
+
+#endif
+
